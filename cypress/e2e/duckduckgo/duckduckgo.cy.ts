dfb0ede915547d40d7fe9aa9fb039c9b2b114b01
webnexusmobile <webnexusmobile@gmail.com>
2024-02-11 14:46:48 +0100

    Use typescript

---
+++ cypress/e2e/duckduckgo/duckduckgo.cy.ts
@@ -0,0 +1,9 @@
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When("I visit duckduckgo.com", () => {
+    cy.visit("https://duckduckgo.com/");
+});
+
+Then("I should see a search bar", () => {
+    cy.findByRole('combobox', {name: 'Search with DuckDuckGo'}).should('be.visible')
+});
